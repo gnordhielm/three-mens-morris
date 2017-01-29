3721615f08f6fe12dc864211b7df35c11a4c9798
Gus Nordhielm <gnordhielm@gmail.com>
2017-01-29 14:05:42 -0800

    add game logic and css rules for place phase

--- js/main.js
+++ js/main.js
@@ -41,21 +41,47 @@ as soon as one player has three consecutive pieces, a popup appears:
 				on click, re-render the game with the names intact.
 */
 
-////////// Global Variables //////////
+////////// Gameplay Global Variables //////////
+
+var playerOne
+var playerTwo
 
 var playerOneName = "Player One"
 var playerTwoName = "Player Two"
 
-////////// Opening Interface //////////
+var playerOnePanel = document.getElementById('playerOnePanel')
+var playerTwoPanel = document.getElementById('playerTwoPanel')
 
-form = document.getElementsByTagName('form')[0]
-formWrapper = document.getElementsByClassName('input')[0]
+var coinTossButton = document.getElementById('coinTossButton')
+var restartButton = document.getElementById('restartButton')
+var restartToken = false
 
-nameOneField = document.getElementById('playerOneName')
-nameTwoField = document.getElementById('playerTwoName')
+var turnIndicator = document.getElementById('indicator')
 
-messageWrapper = document.getElementsByClassName('message')[0]
-instructionsWrapper = document.getElementsByClassName('instructions')[0]
+var boardPositions = {
+	a:'38px',
+	b:'190px',
+	c:'342px',
+	1:'38px',
+	2:'190px',
+	3:'342px'
+}
+
+var playedPieces = 0
+var spaces = document.getElementsByClassName('space')
+var playerOnePieces = document.getElementsByClassName('playerOnePiece')
+var playerTwoPieces = document.getElementsByClassName('playerTwoPiece')
+
+////////// Welcome Interface //////////
+
+var form = document.getElementsByTagName('form')[0]
+var formWrapper = document.getElementsByClassName('input')[0]
+
+var nameOneField = document.getElementById('playerOneName')
+var nameTwoField = document.getElementById('playerTwoName')
+
+var messageWrapper = document.getElementsByClassName('message')[0]
+var instructionsWrapper = document.getElementsByClassName('instructions')[0]
 
 
 form.addEventListener('submit', function(event){
@@ -71,22 +97,23 @@ form.addEventListener('submit', function(event){
 	formWrapper.style.display = 'none'
 })
 
-messageButton = document.getElementById('messageButton')
+var messageButton = document.getElementById('messageButton')
 
 messageButton.addEventListener('click', function(event){
 	messageWrapper.style.display = 'none'
 	instructionsWrapper.style.display = 'block'
 })
 
-instructionsButton = document.getElementById('instructionsButton')
-gameWrapper = document.getElementsByClassName('game')[0]
+var instructionsButton = document.getElementById('instructionsButton')
+var gameWrapper = document.getElementsByClassName('game')[0]
 
 instructionsButton.addEventListener('click', function(event){
 	instructionsWrapper.style.display = 'none'
+	makePlayers()
 	gameWrapper.style.display = 'block'
 })
 
-////////// Set Up Players //////////
+////////// Set Up Game //////////
 
 
 function Player(name) {
@@ -95,29 +122,109 @@ function Player(name) {
 	this.turn = false
 }
 
-var playerOne = new Player(playerOneName)
-var playerTwo = new Player(playerTwoName)
 
-////////// Set Up Board //////////
+function makePlayers() {
+	playerOne = new Player(playerOneName)
+	playerTwo = new Player(playerTwoName)
+	playerOnePanel.getElementsByTagName('h1')[0].innerHTML = playerOneName
+	playerTwoPanel.getElementsByTagName('h1')[0].innerHTML = playerTwoName
+}
 
-var gameBoard = [
-	"a1", "a2", "a3",
-	"b1", "b2", "b3",
-	"c1", "c2", "c3"
-]
+function makeBoard() {
+	for (var i = 0; i < spaces.length; i++) {
+		spaces[i].addEventListener('click', function(event){
+			if (playerOne.turn) {
+				playPlaceTurn(playerOne, this.id)
+			} else {
+				playPlaceTurn(playerTwo, this.id)
+			}
+			if (playedPieces === 6) startMovePhase()
+		})
+	}
+}
 
-function coinToss(pOne, pTwo) {
+function coinToss() {
 	switch(Math.floor(Math.random() * 2)) {
 		case 0:
-			pOne.turn = true
+			playerOne.turn = true
 			break;
 		case 1:
-			pTwo.turn = true
+			playerTwo.turn = true
 			break;
 	}
 }
 
+coinTossButton.addEventListener('click', function(event){
+	coinToss()
+	document.getElementById('coinToss').style.display = 'none'
+	document.getElementById('gameBoard').style.display = 'block'
+	if (!restartToken) makeBoard()
+	if (playerOne.turn) {
+		turnIndicator.className = "left"
+	} else {
+		turnIndicator.className = "right"
+	}
+
+})
+
+restartButton.addEventListener('click', function(event){
+	// board doesn't get made again
+	restartToken = true
+	// turn indicator is offscreen
+	turnIndicator.className = "off"
+	// pieces are up top
+	for (var i = 0; i < playerOnePieces.length; i++) {
+		playerOnePieces[i].style.top = '-100px'
+	}
+	for (var i = 0; i < playerTwoPieces.length; i++) {
+		playerTwoPieces[i].style.top = '-100px'
+	}
+	playerOnePieces[0].style.left = '133px'
+	playerOnePieces[1].style.left = '73px'
+	playerOnePieces[2].style.left = '13px'
+	playerTwoPieces[0].style.left = '360px'
+	playerTwoPieces[1].style.left = '300px'
+	playerTwoPieces[2].style.left = '240px'
+	// player.pieces are all null
+	playerOne.pieces = [null, null, null]
+	playerTwo.pieces = [null, null, null]
+	// player.turn are both false
+	playerOne.turn = false
+	playerTwo.turn = false
+	// coin toss, not game board, is visible
+	document.getElementById('coinToss').style.display = 'block'
+	document.getElementById('gameBoard').style.display = 'none'
+})
+
 ////////// Start Game //////////
+
+// question - I specifically put checkForWin AFTER updateBoard, so that the
+//   board would reflect a win before announcing it... 
+//   why is it firing an alert, and halting the animation?
+
+function playPlaceTurn(player, destination) {
+	if (isLegalPlace(destination)) {
+		placePiece(player, destination)
+		updateBoard(player)
+		if (checkForWin(player)) {
+			winner(player)
+		} else {
+			nextTurn()
+		}
+	} else {
+		alert("That spot is taken!")
+	}
+}
+
+function updateBoard(player) {
+	playerPieces = playerOne.turn ? playerOnePieces : playerTwoPieces
+	for (var i = 0; i < playerPieces.length; i++) {
+		if (player.pieces[i]) {
+			playerPieces[i].style.top = boardPositions[player.pieces[i][0]]
+			playerPieces[i].style.left = boardPositions[player.pieces[i][1]]
+		}
+	}
+}
 
 function checkForWin(player) {
 	if (player.pieces.indexOf(null) !== -1) {
@@ -131,14 +238,14 @@ function checkForWin(player) {
 			   player.pieces[0][1] === player.pieces[2][1]) {
 		// vertical win
 		return true
-	} else if (player.pieces[0] === 'a1' && 
-			   player.pieces[0] === 'b2'&& 
-			   player.pieces[0] === 'c3') {
+	} else if (player.pieces.includes('a1') && 
+			   player.pieces.includes('b2')&& 
+			   player.pieces.includes('c3')) {
 		// l > r diagonal win
 		return true
-	} else if (player.pieces[0] === 'a3' && 
-			   player.pieces[0] === 'b2'&& 
-			   player.pieces[0] === 'c1') {
+	} else if (player.pieces.includes('a3') && 
+			   player.pieces.includes('b2')&& 
+			   player.pieces.includes('c1')) {
 		//r > l diagonal win
 		return true
 	} else {
@@ -146,13 +253,15 @@ function checkForWin(player) {
 	}
 }
 
-function nextTurn(pOne, pTwo) {
-	if (pOne.turn === true) {
-		pOne.turn === false
-		pTwo.turn === true
+function nextTurn() {
+	if (playerOne.turn === true) {
+		playerOne.turn = false
+		playerTwo.turn = true
+		turnIndicator.className = "right"
 	} else {
-		pOne.turn === true
-		pTwo.turn === false
+		playerOne.turn = true
+		playerTwo.turn = false
+		turnIndicator.className = "left"
 	}
 }
 
@@ -170,9 +279,29 @@ function isLegalPlace(destination) {
 
 function placePiece(player, destination) {
 	player.pieces[player.pieces.indexOf(null)] = destination
+	playedPieces ++
 }
 
 ////////// Move Phase //////////
+
+function startMovePhase() {
+	// for loop to give pieces onclick trigger. Differentiate between black and white.
+	// make a variable to hold a move as it's built up - location and destination
+	//collapse the holder div
+}
+
+function playMoveTurn(player, location, destination) {
+	if (isLegalPlace(destination)) {
+		placePiece(player, destination)
+		if (checkForWin(player)) {
+			winner(player)
+		} else {
+			nextTurn()
+		}
+	} else {
+		alert("You can't move there!")
+	}
+}
 
 function isLegalMove(location, destination) {
 	if (playerOne.pieces.indexOf(destination) !== -1) {
@@ -196,7 +325,9 @@ function movePiece(player, location, destination) {
 	player.pieces[player.pieces.indexOf(location)] = destination
 }
 
-
+function winner(player) {
+	alert(player.name + " wins the game!")
+}
 
 
 
